089e9b9f63d97f5ccc1cc764aee451602ee0b69f
Lorena Rincon <lorec.rincon@gmail.com>
2023-08-02 10:19:28 -0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Attribution from "./components/Attribution";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Attribution />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Attribution() {
+  return (
+    <div className="attribution hidden lg:block">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/lorenacrincon" target="_blank">
+        Lorena Rincón
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Attribution;
